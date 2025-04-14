65b6ae62528ba041bdd431faa8f569d68811e166
Ansh98755 <ayushmehendiratta3390@gmail.com>
2025-04-14 16:38:09 +0530

    Training the dataset

--- handwritten.py
+++ handwritten.py
@@ -1,4 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+
+(x_train, y_train),(x_test, y_test)=tf.keras.datasets.mnist.load_data()
+
+x_train = x_train/255.0
+x_test = x_test/255.0
+x_train = x_train.reshape(-1, 28 * 28)
+x_test = x_test.reshape(-1, 28 * 28)
